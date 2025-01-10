7aa6f59fba40639a43c1f117b98ae4f5102232ed
Huseyin0P <113569641+Huseyin0P@users.noreply.github.com>
2025-01-10 11:41:45 -0600

    Add files via upload

---
+++ script/signup.js
@@ -0,0 +1,12 @@
+let su = document.getElementById("su")
+let li = document.getElementById("li")
+function signupopen(){
+    su.style.display = "block"
+    su.style.zIndex = 1
+    li.style.zIndex = 0
+}
+function loginopen(){
+    li.style.display = "block"
+    su.style.zIndex = 0
+    li.style.zIndex = 1
+}
